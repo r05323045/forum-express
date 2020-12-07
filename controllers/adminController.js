4fa2dc8bc98c0a785d133fe8bb9898f3fd916a2d
r05323045 <r05323045@ntu.edu.tw>
2020-12-07 22:53:29 +0800

    refine A17: add error handling and improve permission management

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,6 @@
 const imgur = require('imgur-node-api')
 const db = require('../models')
+const helpers = require('../_helpers')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 const Restaurant = db.Restaurant
 const User = db.User
@@ -161,6 +162,7 @@ const adminController = {
 
   getUsers: (req, res) => {
     return User.findAll({ raw: true }).then(users => {
+      users.forEach(user => { user.isMe = user.id === helpers.getUser(req).id })
       return res.render('admin/users', { users: users })
     })
   },
@@ -168,13 +170,23 @@ const adminController = {
   putUsers: (req, res) => {
     return User.findByPk(req.params.id)
       .then((user) => {
-        user.update({
-          isAdmin: req.body.isAdmin === '0' ? 0 : 1
-        })
-          .then((user) => {
-            req.flash('success_messages', `${user.name} was successfully to update`)
-            res.redirect('/admin/users')
+        if (user.id !== helpers.getUser(req).id) {
+          return user.update({
+            isAdmin: req.body.isAdmin === '0' ? 0 : 1
           })
+        } else {
+          req.flash('error_messages', 'permission denied')
+          res.redirect('/admin/users')
+        }
+      })
+      .then((user) => {
+        req.flash('success_messages', `${user.name} was successfully to update`)
+        res.redirect('/admin/users')
+      })
+      .catch(err => {
+        console.log(err)
+        req.flash('error_messages', 'Sorry, something went wrong')
+        res.redirect('/admin/users')
       })
   }
 }
